7958f0534932fb1760030f61f15d67fb2b61a8bf
bila <zaskia salsabilazaskia68@gmail.com>
2026-02-03 11:05:46 +0700

    update

--- main.js
+++ main.js
@@ -1,36 +1,58 @@
+ let databarang = [] // array global untuk menyimpan data barang 
+
 // fungsi format rupiah 1.000.000
 function rupiah(angka) {
   return angka.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
 }
 
-function process() {
+function proses() {
+  // reset data barang setiap kali proses dijalankan
+  dataBarang = []
+  
+  // reset output setiap kali proses dijalankan 
+  document.getElementById("hasil").innerHTML = ""
+  
 let JUMLAH_BARANG =  Number (prompt("JUMLAH_BARANG")) 
   
-  let total = 0;
-  let output = "";
-  
-  // LOOP INPUT HARGA BARANG
+   // LOOP INPUT HARGA BARANG
   for (let i = 1; i <= JUMLAH_BARANG; i++) {
-    let harga = Number(prompt("Masukkan harga barang ke-" + i));
-    total += harga;
-    
-    output += "Barang ke-" + i + ": Rp " + rupiah(harga) + "<br>";
+    let harga = Number(prompt("Masukkan harga barang ke-" + i))
+    dataBarang.push(harga)
   }
   
+  // aktifkan tombol cetak
+  document.getElementById("cetak").disabled = false
+ 
+ // menampilkan info barang yang sudah diinput
+ document.getElementById("hasil").innerHTML = "data barang berhasil diinput.(jumlah barang: " + dataBarang.length + ")"
+  }
+
+  function cetak() {
+    let total = 0
+    let output = ""
+
+ // loop input harga barang dari array global dataBarang
+ for (let i = 0; i <dataBarang.length; i++){
+   let harga= dataBarang [i]
+   total += harga
+   
+   output +="Barang ke-" + (i + 1) + ":Rp " + rupiah(harga) + "<br>"
+ }
+ 
   // HITUNG DISKON
-  let diskon = 0;
+  let diskon = 0
   if (total > 500000) {
-    diskon = total * 0.2;
+    diskon = total * 0.2
   }
-  
-  let setelahDiskon = total - diskon;
-  
+
+  let setelahDiskon = total - diskon
+
   // HITUNG PPN
-  let ppn = setelahDiskon * 0.1;
-  
+  let ppn = setelahDiskon * 0.1
+
   // TOTAL BAYAR
-  let totalBayar = setelahDiskon + ppn;
-  
+  let totalBayar = setelahDiskon + ppn
+
   // OUTPUT RINGKASAN
   output += `
     <div class="ringkasan">
@@ -39,7 +61,9 @@ let JUMLAH_BARANG =  Number (prompt("JUMLAH_BARANG"))
       PPN 10%: Rp ${rupiah(ppn)}<br>
       Total bayar: Rp ${rupiah(totalBayar)}
     </div>
-  `;
-  
-  document.getElementById("hasil").innerHTML = output;
-}
+  `
+
+  document.getElementById("hasil").innerHTML = output
+      
+    }
+    
